9d6b9bc598ff111db1779a13355d6c4d1db263bd
Muqtadirat <muqtadir.my@gmail.com>
2024-03-29 15:23:35 +0100

    feat: card comp

---
+++ components/shared/Card.jsx
@@ -0,0 +1,16 @@
+const Card = ({ children, url }) => {
+  return (
+    <div
+      className="bg-cover bg-center overflow-hidden rounded-md h-48 sm:h-[24.8rem] hover:cursor-pointer w-[8rem] sm:w-[14.75rem] hover:w-[22.5rem] transition-width duration-300 ease-in-out"
+      style={{ backgroundImage: `url(${url})` }}
+    >
+      <div className="bg-primary-gray/65 hover:bg-primary-gray/25 w-full h-full flex items-end justify-center transition-background duration-300 ease-in-out">
+        <p className="mb-20 text-white text-base sm:text-xl font-medium sm:w-[8.25rem] text-center">
+          {children}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
