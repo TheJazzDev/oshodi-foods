b82607452165cc21325b992ceef871f7b2f5fca0
Muqtadirat <muqtadir.my@gmail.com>
2024-03-31 12:11:03 +0100

    fix: font

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,11 @@
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "@/styles/globals.css";
 import { Navbar, Footer, Sidebar, MobileFooter } from "@/layout";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600"],
+});
 
 export const metadata = {
   title: "Oshodi Foods",
@@ -12,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={poppins.className}>
         <Navbar />
         <Sidebar />
         <main className="py-6 sm:py-12">{children}</main>
